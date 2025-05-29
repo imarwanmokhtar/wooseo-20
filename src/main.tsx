b7cd911f6bfa11bb7afdee92b4db4f2076fcaebf
imarwanmokhtar <marwanmokhtar000@gmail.com>
2025-05-29 21:20:15 +0200

    vercel ll

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import App from './App.tsx'
 import './index.css'
-{
-    (
-        <>
-            <SpeedInsights />
-            <Analytics />
-        </>
-    )
-}
+
 createRoot(document.getElementById("root")!).render(<App />);
